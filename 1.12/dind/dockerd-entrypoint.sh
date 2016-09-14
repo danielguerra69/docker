2e7b354ae1406f720792d18bdc39619de3b86c37
danielguerra69 <daniel.guerra69@gmail.com>
2016-09-14 16:04:04 +0200

    problems fixed
    Everything works well.
    Improved dockerd-entrypoint, all restartable + keygen. Pip cleanup
    added. Expose added port 22 for sshd. Remove any keys that might have
    been installed.

--- 1.12/dind/dockerd-entrypoint.sh
+++ 1.12/dind/dockerd-entrypoint.sh
@@ -16,17 +16,21 @@ if [ "$1" = 'docker' -a "$2" = 'daemon' ]; then
 fi
 
 #SSHD
-# generate fresh rsa key
-ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-# generate fresh dsa key
-ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+# prepare keys
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+	# generate fresh rsa key
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+fi
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+	# generate fresh dsa key
+	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
 #prepare run dir
-mkdir -p /var/run/sshd
-# prepare config file for key based auth
-sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config
-sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config
-sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
-sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config
+if [ ! -d "/var/run/sshd" ]; then
+	mkdir -p /var/run/sshd
+fi
 
+# start sshd
+/usr/sbin/sshd -D &
 
 exec "$@"
--- 1.12/docker-entrypoint.sh
+++ 1.12/docker-entrypoint.sh
@@ -11,9 +11,4 @@ if docker "$1" --help > /dev/null 2>&1; then
 	set -- docker "$@"
 fi
 
-# if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
-if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
-	export DOCKER_HOST='tcp://docker:2375'
-fi
-
 exec "$@"
